751344723accb4306610718a999d1c81f3a1bfa6
Alerrando <alerrando2@gmail.com>
2023-08-07 16:24:50 -0300

    add BubbleButton component

---
+++ src/components/BubbleButton.tsx
@@ -0,0 +1,14 @@
+import React, { ReactNode, ComponentProps } from "react";
+
+export interface BubbleButtonProps extends ComponentProps<"Button"> {
+    children: ReactNode
+} 
+
+export function BubbleButton(props: BubbleButtonProps) {
+    return(
+        <button
+            className="p-2 text-zinc-200 text-sm flex items-center gap-1.5 font-medium leading-none hover:text-zinc-50 hover:bg-zinc-600"
+            {...props}
+        />
+    )
+}
--- src/components/Editor.tsx
+++ src/components/Editor.tsx
@@ -5,8 +5,9 @@ import { initialContet } from "./InitialContent";
 import CodeBlockLowlight from "@tiptap/extension-code-block-lowlight";
 import js from "highlight.js/lib/languages/javascript";
 import html from "highlight.js/lib/languages/xml";
-
+import { RxFontBold, RxFontItalic, RxStrikethrough, RxCode } from "react-icons/rx"
 import "highlight.js/styles/panda-syntax-dark.css"
+import { BubbleButton } from "./BubbleButton";
 
 lowlight.registerLanguage("html", html);
 lowlight.registerLanguage("js", js);
@@ -39,8 +40,20 @@ export function Editor() {
             />
 
             { editor && (
-                <BubbleMenu editor={editor}>
-                    <button>Bold</button>
+                <BubbleMenu className="bg-zinc-700 shadow-xl border border-zinc-600 shadow-black/20 rounded-lg overflow-hidden flex divide-x divide-zinc-600" editor={editor}>
+                    <BubbleButton>
+                        <RxFontBold className="w-4 h-4" />
+                    </BubbleButton>
+                    <BubbleButton>
+                        <RxFontItalic className="w-4 h-4" />
+                    </BubbleButton>
+                    <BubbleButton>
+                        <RxStrikethrough className="w-4 h-4" />
+                    </BubbleButton>
+
+                    <BubbleButton>
+                        <RxCode className="w-4 h-4" />
+                    </BubbleButton>
                 </BubbleMenu>
             ) }
         </>
