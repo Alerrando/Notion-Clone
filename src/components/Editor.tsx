af8c0ac1f5deb64e2c2e65365f85e8f3da3a6705
Alerrando <alerrando2@gmail.com>
2023-08-07 17:02:36 -0300

    add functions floatingmenu

--- src/components/Editor.tsx
+++ src/components/Editor.tsx
@@ -1,13 +1,13 @@
-import { useEditor, EditorContent, BubbleMenu } from "@tiptap/react";
-import StarterKit from "@tiptap/starter-kit";
-import { lowlight } from 'lowlight';
-import { initialContet } from "./InitialContent";
 import CodeBlockLowlight from "@tiptap/extension-code-block-lowlight";
+import { BubbleMenu, EditorContent, FloatingMenu, useEditor } from "@tiptap/react";
+import StarterKit from "@tiptap/starter-kit";
 import js from "highlight.js/lib/languages/javascript";
 import html from "highlight.js/lib/languages/xml";
-import { RxFontBold, RxFontItalic, RxStrikethrough, RxCode, RxChevronDown, RxChatBubble } from "react-icons/rx"
-import "highlight.js/styles/panda-syntax-dark.css"
+import "highlight.js/styles/panda-syntax-dark.css";
+import { lowlight } from 'lowlight';
+import { RxChatBubble, RxChevronDown, RxCode, RxFontBold, RxFontItalic, RxStrikethrough } from "react-icons/rx";
 import { BubbleButton } from "./BubbleButton";
+import { initialContet } from "./InitialContent";
 
 lowlight.registerLanguage("html", html);
 lowlight.registerLanguage("js", js);
@@ -39,20 +39,64 @@ export function Editor() {
                 className="max-w-[700px] mx-auto pt-16 prose prose-invert"
             />
 
-            { editor && (
+            {editor && (
+                <FloatingMenu
+                    editor={editor}
+                    shouldShow={({ state }) => {
+                        const { $from } = state.selection;
+
+                        const currentLineText = $from.nodeBefore?.textContent;
+
+                        return currentLineText === "/";
+                    }}
+
+                    className="bg-zinc-700 py-2 px-1 gap-1 shadow-xl border border-zinc-600 shadow-black/20 rounded-lg overflow-hidden flex flex-col"
+                >
+                    <button 
+                        className="flex items-center gap-2 p-1 rounded min-w-[280px] hover:bg-zinc-600"
+                    >
+                        <img 
+                            src="https://www.notion.so/images/blocks/text/en-US.png" 
+                            alt="Text"
+                            className="w-12 border border-zinc-600 rounded"
+                        />
+                        <div className="flex flex-col text-left">
+                            <span className="text-sm">Text</span>
+                            <span className="text-xs text-zinc-400">Just start writing with plain text.</span>
+                        </div>
+                    </button>
+
+                    <button 
+                        className="flex items-center gap-2 p-1 rounded min-w-[280px] hover:bg-zinc-600"
+                        onClick={() => editor.chain().focus().toggleHeading({ level: 1 }).run()}
+                    >
+                        <img 
+                            src="https://www.notion.so/images/blocks/header.57a7576a.png" 
+                            alt="Heading"
+                            className="w-12 border border-zinc-600 rounded"
+                        />
+                        <div className="flex flex-col text-left">
+                            <span className="text-sm">Heading 1</span>
+                            <span className="text-xs text-zinc-400">Big section heading.</span>
+                        </div>
+                    </button>
+                </FloatingMenu>
+            )}
+
+            {editor && (
                 <BubbleMenu className="bg-zinc-700 shadow-xl border border-zinc-600 shadow-black/20 rounded-lg overflow-hidden flex divide-x divide-zinc-600" editor={editor}>
                     <BubbleButton>
-                            Text
-                            <RxChevronDown className="w-4 h-4" />
+                        Text
+                        <RxChevronDown className="w-4 h-4" />
                     </BubbleButton>
 
                     <BubbleButton>
-                            Comment
-                            <RxChatBubble className="w-4 h-4" />
+                        Comment
+                        <RxChatBubble className="w-4 h-4" />
                     </BubbleButton>
-                    
+
                     <div className="flex items-center">
-                        <BubbleButton 
+                        <BubbleButton
                             onClick={() => editor.chain().focus().toggleBold().run()}
                             data-active={editor.isActive("bold")}
                         >
@@ -81,7 +125,7 @@ export function Editor() {
                         </BubbleButton>
                     </div>
                 </BubbleMenu>
-            ) }
+            )}
         </>
     )
 }
