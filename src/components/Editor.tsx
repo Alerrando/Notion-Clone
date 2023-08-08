050233ef038f1ddf322e46daec5ff48f8a1c51d5
Alerrando <alerrando2@gmail.com>
2023-08-08 13:51:05 -0300

    add ToggleGroup from Editor and BubbleButton delete

--- src/components/Editor.tsx
+++ src/components/Editor.tsx
@@ -1,3 +1,4 @@
+import * as ToggleGroup from "@radix-ui/react-toggle-group";
 import CodeBlockLowlight from "@tiptap/extension-code-block-lowlight";
 import {
   BubbleMenu,
@@ -18,7 +19,6 @@ import {
   RxFontItalic,
   RxStrikethrough,
 } from "react-icons/rx";
-import { BubbleButton } from "./BubbleButton";
 import { initialContet } from "./InitialContent";
 
 lowlight.registerLanguage("html", html);
@@ -27,6 +27,8 @@ lowlight.registerLanguage("js", js);
 export interface EditorProps {}
 
 export function Editor() {
+  const toggleGroupItemClasses =
+    "p-2 text-zinc-200 text-sm flex items-center gap-1.5 font-medium leading-none hover:text-zinc-50 hover:bg-zinc-600 data-[active=true]:text-violet-400";
   const editor = useEditor({
     extensions: [
       StarterKit,
@@ -34,6 +36,9 @@ export function Editor() {
         lowlight,
       }),
     ],
+    onUpdate(editor) {
+      editor.editor.getJSON();
+    },
     content: initialContet,
     editorProps: {
       attributes: {
@@ -97,49 +102,73 @@ export function Editor() {
       )}
 
       {editor && (
-        <BubbleMenu
-          className="bg-zinc-700 shadow-xl border border-zinc-600 shadow-black/20 rounded-lg overflow-hidden flex divide-x divide-zinc-600"
-          editor={editor}
-        >
-          <BubbleButton>
-            Text
-            <RxChevronDown className="w-4 h-4" />
-          </BubbleButton>
-
-          <BubbleButton>
-            Comment
-            <RxChatBubble className="w-4 h-4" />
-          </BubbleButton>
-
-          <div className="flex items-center">
-            <BubbleButton
-              onClick={() => editor.chain().focus().toggleBold().run()}
-              data-active={editor.isActive("bold")}
+        <BubbleMenu editor={editor}>
+          <ToggleGroup.Root
+            className="bg-zinc-700 shadow-xl border border-zinc-600 shadow-black/20 rounded-lg overflow-hidden flex divide-x divide-zinc-600"
+            type="single"
+            defaultValue="text"
+            aria-label="Menu Bubble Item"
+          >
+            <ToggleGroup.Item
+              value="text"
+              aria-label="Text Item"
+              className={toggleGroupItemClasses}
             >
-              <RxFontBold className="w-4 h-4" />
-            </BubbleButton>
+              Text
+              <RxChevronDown className="w-4 h-4" />
+            </ToggleGroup.Item>
 
-            <BubbleButton
-              onClick={() => editor.chain().focus().toggleItalic().run()}
-              data-active={editor.isActive("italic")}
+            <ToggleGroup.Item
+              value="comment"
+              aria-label="Comment Item"
+              className={toggleGroupItemClasses}
             >
-              <RxFontItalic className="w-4 h-4" />
-            </BubbleButton>
+              Comment
+              <RxChatBubble className="w-4 h-4" />
+            </ToggleGroup.Item>
 
-            <BubbleButton
-              onClick={() => editor.chain().focus().toggleStrike().run()}
-              data-active={editor.isActive("strike")}
-            >
-              <RxStrikethrough className="w-4 h-4" />
-            </BubbleButton>
+            <div className="flex items-center">
+              <ToggleGroup.Item
+                value="font-bold"
+                aria-label="Font Bold Item"
+                onClick={() => editor.chain().focus().toggleBold().run()}
+                data-active={editor.isActive("bold")}
+                className={toggleGroupItemClasses}
+              >
+                <RxFontBold className="w-4 h-4" />
+              </ToggleGroup.Item>
 
-            <BubbleButton
-              onClick={() => editor.chain().focus().toggleCode().run()}
-              data-active={editor.isActive("code")}
-            >
-              <RxCode className="w-4 h-4" />
-            </BubbleButton>
-          </div>
+              <ToggleGroup.Item
+                value="font-italic"
+                aria-label="Font Italic Item"
+                onClick={() => editor.chain().focus().toggleItalic().run()}
+                data-active={editor.isActive("italic")}
+                className={toggleGroupItemClasses}
+              >
+                <RxFontItalic className="w-4 h-4" />
+              </ToggleGroup.Item>
+
+              <ToggleGroup.Item
+                value="font-strike"
+                aria-label="Font Strike Item"
+                onClick={() => editor.chain().focus().toggleStrike().run()}
+                data-active={editor.isActive("strike")}
+                className={toggleGroupItemClasses}
+              >
+                <RxStrikethrough className="w-4 h-4" />
+              </ToggleGroup.Item>
+
+              <ToggleGroup.Item
+                value="font-code"
+                aria-label="Font Code Item"
+                onClick={() => editor.chain().focus().toggleCode().run()}
+                data-active={editor.isActive("code")}
+                className={toggleGroupItemClasses}
+              >
+                <RxCode className="w-4 h-4" />
+              </ToggleGroup.Item>
+            </div>
+          </ToggleGroup.Root>
         </BubbleMenu>
       )}
     </>
