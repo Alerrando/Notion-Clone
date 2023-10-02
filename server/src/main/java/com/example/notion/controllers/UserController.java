4a1c0f62d1843cdb3ef94b3c3f63a5c3fd229378
Alerrando <alerrando2@gmail.com>
2023-10-02 16:52:47 -0300

    fix: fixing CrossOrigin to UserController

--- server/src/main/java/com/example/notion/controllers/UserController.java
+++ server/src/main/java/com/example/notion/controllers/UserController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-@CrossOrigin(origins = "https://localhost:5173")
+@CrossOrigin(origins = "http://localhost:5173")
 @RestController
 @RequestMapping(value = "/user")
 public class UserController {
