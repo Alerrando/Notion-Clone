37841680d7d9ad26c79c37415d7f3e946bf69161
Alerrando <alerrando2@gmail.com>
2023-10-04 16:40:30 -0300

    feat: add verifyToken and getAllClaimsFromToken from JWTTokenProvider

--- server/src/main/java/com/example/notion/security/JWTTokkenProvider.java
+++ server/src/main/java/com/example/notion/security/JWTTokkenProvider.java
@@ -1,6 +1,7 @@
 package com.example.notion.security;
 
 import com.example.notion.entities.User;
+import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.security.Keys;
@@ -25,5 +26,34 @@ public class JWTTokkenProvider {
                 .compact();
 
         return jwtToken;
+    }
+
+    static public boolean verifyToken(String token){
+        try {
+            Jwts.parserBuilder()
+                    .setSigningKey(SECRET_KEY)
+                    .build()
+                    .parseClaimsJws(token)
+                    .getSignature();
+
+            return true;
+        } catch (Exception e){
+            return false;
+        }
+    }
+
+    static public Claims getAllClaimsFromToken(String token){
+        Claims claims = null;
+
+        try {
+            claims = Jwts.parserBuilder()
+                    .setSigningKey(SECRET_KEY)
+                    .build()
+                    .parseClaimsJws(token)
+                    .getBody();
+            return claims;
+        } catch (Exception e){
+            return null;
+        }
     }
 }
